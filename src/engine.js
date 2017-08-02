6c3a3eadf9c4f56ac7df144cd41e74d0947946e3
sz-piotr <szlachciak.piotr@gmail.com>
2017-08-02 22:39:26 +0200

    change engine to provide basic functionality

--- src/engine.js
+++ src/engine.js
@@ -2,50 +2,38 @@ import { Entity } from './entity'
 
 export class Engine {
   constructor() {
-    this._entities = null
-    this._components = null
-    this._keyLength = 0
+    this.entities = null
+    this.dirtyEntites = []
+
+    this.addToDirty = this.addToDirty.bind(this)
   }
 
   createEntity() {
-    if(this._components === null) {
-      throw new Error('Engine.defineComponents() must be called before creating any entities.')
-    }
-
-    const entity = new Entity(this._entities, this._keyLength)
-    this._entities = entity
+    const entity = new Entity(this.entities, this.addToDirty)
+    this.entities = entity
 
     return entity
   }
 
   removeEntity(entity) {
-    const next = entity._next
+    const next = entity.next
 
-    if(entity._next !== null) {
-      entity._next._prev = entity._prev
-      entity._next = null
+    if(entity.next !== null) {
+      entity.next.prev = entity.prev
+      entity.next = null
     }
 
-    if(entity._prev !== null) {
-      entity._prev._next = entity._next
-      entity._prev = null
+    if(entity.prev !== null) {
+      entity.prev.next = entity.next
+      entity.prev = null
     }
 
-    if(this._entities = entity) {
-      this._entities = next
+    if(this.entities = entity) {
+      this.entities = next
     }
   }
 
-  defineComponents(components) {
-    if(this._components !== null) {
-      throw new Error('Engine.defineComponents() can only be called once on every engine instance.')
-    }
-
-    if(!components.constructor === Array) {
-      throw new Error('Engine.defineComponents() only accepts an Array as argument.')
-    }
-
-    this._components = components
-    this._keyLength = Math.ceil(components.length / 2)
+  addToDirty(entity) {
+    this.dirtyEntites.push(entity)
   }
 }
--- src/engine.test.js
+++ src/engine.test.js
@@ -1,7 +1,9 @@
 import { Engine } from './engine'
 
-describe('Engine', () => {
+describe('Engine.constructor()', () => {
   test('should be a constructor', () => {
-    expect(() => new Engine()).not.toThrow()
+    expect(() => {
+      new Engine()
+    }).not.toThrow()
   })
 })
