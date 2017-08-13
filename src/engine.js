7fbf752ddabdbc0acb60fbc39c4f4b9044f3268f
sz-piotr <szlachciak.piotr@gmail.com>
2017-08-13 21:45:22 +0200

    reimplement engine

--- src/engine.js
+++ src/engine.js
@@ -4,37 +4,94 @@ export class Engine {
   constructor() {
     this.entities = null
     this.dirtyEntites = []
-    this.queries = []
+    this.systems = []
+    this.componentIdMap = []
+    this.componentsCount = 0
 
-    this.addToDirty = this.addToDirty.bind(this)
+    this.configurable = true
+    this.running = false
+
+    this.onEntityKeyChange = this.onEntityKeyChange.bind(this)
   }
 
   createEntity() {
-    const entity = new Entity(this.entities, this.addToDirty)
+    this.configurable = false
+    const entity = new Entity(
+      this.componentsCount,
+      this.componentIdMap,
+      this.onEntityKeyChange
+    )
+    entity.next = this.entities
+    if(this.entities !== null) {
+      this.entities.prev = entity
+    }
     this.entities = entity
 
     return entity
   }
 
-  addToDirty(entity) {
-    this.dirtyEntites.push(entity)
-  }
-
   removeEntity(entity) {
     const next = entity.next
+    const prev = entity.prev
 
-    if(entity.next !== null) {
-      entity.next.prev = entity.prev
-      entity.next = null
+    if(next !== null) {
+      next.prev = entity.prev
+    }
+    if(prev !== null) {
+      prev.next = entity.next
     }
 
-    if(entity.prev !== null) {
-      entity.prev.next = entity.next
-      entity.prev = null
-    }
+    entity.next = null
+    entity.prev = null
 
     if(this.entities = entity) {
       this.entities = next
     }
   }
+
+  onEntityKeyChange(entity) {
+    this.dirtyEntites.push(entity)
+  }
+
+  registerSystem({ query, sort, update, processEntity }) {
+    if(!this.configurable) {
+      throw new Error('Cannot register system after entities have been added or the engine was started.')
+    }
+
+    if(bothOrNone(update, processEntity)) {
+      throw new Error('Exactly one of "update" and "processEntity" must be defined on a system.')
+    }
+
+    if(!query && processEntity) {
+      throw new Error('"processEntity" can only be used with a query.')
+    }
+
+    this.systems.push({
+      query: query ? query.bake(this) : false,
+      sort,
+      update: update || function(entities, timeDelta, engine) {
+        for(let i = 0; i < entities.length; ++i) {
+          processEntity(entities[i], timeDelta, engine)
+        }
+      }
+    })
+  }
+
+  registerComponent(component) {
+    if(!this.configurable) {
+      throw new Error('Cannot register component after entities have been added or the engine was started.')
+    }
+
+    while(component.id >= this.componentIdMap.length) {
+      this.componentIdMap.push(null)
+    }
+    const index = this.componentIdMap[component.id]
+    if(index === null) {
+      this.componentIdMap[component.id] = this.componentsCount++
+    }
+  }
+}
+
+function bothOrNone(a, b) {
+  return (a && b) || (!a && !b)
 }
--- src/engine.test.js
+++ src/engine.test.js
@@ -1,9 +1,58 @@
 import { Engine } from './engine'
+import { Entity } from './entity'
+import { component } from './component'
 
-describe('Engine.constructor()', () => {
-  test('should be a constructor', () => {
-    expect(() => {
-      new Engine()
-    }).not.toThrow()
+describe('Engine', () => {
+  test('constructor() should be a constructor', () => {
+    expect(() => new Engine()).not.toThrow()
   })
+
+  test('createEntity() should create an entity', () => {
+    expect(new Engine().createEntity()).toBeInstanceOf(Entity)
+  })
+
+  test('createEntity() should form a linked list', () => {
+    const engine = new Engine()
+    const e1 = engine.createEntity()
+    const e2 = engine.createEntity()
+    const e3 = engine.createEntity()
+
+    expect(e1.next).toBe(null)
+    expect(e2.next).toBe(e1)
+    expect(e3.next).toBe(e2)
+
+    expect(e1.prev).toBe(e2)
+    expect(e2.prev).toBe(e3)
+    expect(e3.prev).toBe(null)
+  })
+
+  test('createEntity() should disable configuration', () => {
+    const engine = new Engine()
+    expect(engine.configurable).toBe(true)
+    engine.createEntity()
+    expect(engine.configurable).toBe(false)
+  })
+
+  test('removeEntity() should remove the entity', () => {
+    const engine = new Engine()
+    const e1 = engine.createEntity()
+    const e2 = engine.createEntity()
+    const e3 = engine.createEntity()
+
+    engine.removeEntity(e2)
+
+    expect(e1.next).toBe(null)
+    expect(e3.next).toBe(e1)
+
+    expect(e1.prev).toBe(e3)
+    expect(e3.prev).toBe(null)
+
+    engine.removeEntity(e1)
+
+    expect(e1.next).toBe(null)
+    expect(e1.prev).toBe(null)
+  })
+
+  // TODO: Test registerSystem
+  // TODO: Test registerComponent
 })
