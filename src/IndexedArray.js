5b9358298576d3baef12dec17d4441fc23cc081f
sz-piotr <szlachciak.piotr@gmail.com>
2017-12-16 13:04:25 +0100

    query changes

---
+++ src/IndexedArray.js
@@ -0,0 +1,27 @@
+export class IndexedArray {
+  constructor () {
+    this._indices = {}
+    this.elements = []
+  }
+
+  put (object) {
+    this._indices[object.id] = this.elements.length
+    this.elements.push(object)
+  }
+
+  has (object) {
+    return this._indices[object.id] !== undefined
+  }
+
+  remove (object) {
+    const index = this._indices[object.id]
+    if (index !== undefined) {
+      delete this._indices[object.id]
+      const otherObject = this._entities.pop()
+      if (otherObject !== object) {
+        this.elements[index] = otherObject
+        this._indices[otherObject.id] = index
+      }
+    }
+  }
+}
