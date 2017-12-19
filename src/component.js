c700ab3bf3a3dd72d2897b659ec6420177133f06
sz-piotr <szlachciak.piotr@gmail.com>
2017-12-19 10:33:55 +0100

    refactor component code

--- src/component.js
+++ src/component.js
@@ -1,18 +1,20 @@
+import { forEach, assert } from './utils'
+
 export function createComponent (fields, id) {
   let body = ''
-  for (let i = 0; i < fields.length; i++) {
-    let field = fields[i]
-    if (/^[a-zA-Z]\w*$/.exec(field)) {
-      body += `this.${field}=${field};`
-    } else {
-      throw new Error('Invalid identifier: ' + field)
-    }
-  }
+  forEach(fields, field => {
+    assert(isValid(field), 'Invalid identifier: ' + field)
+    body += `this.${field}=${field};`
+  })
   /* eslint-disable no-new-func */
   return decorate(new Function(fields, body), id)
 }
 
-export function decorate (constructor, id) {
+function isValid (field) {
+  return /^[a-zA-Z]\w*$/.exec(field)
+}
+
+function decorate (constructor, id) {
   constructor.id = id
   constructor.prototype._id = id
   return constructor
