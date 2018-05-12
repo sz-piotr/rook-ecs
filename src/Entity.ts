609aa5218c643ad893908022165f45389bd86777
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2018-05-12 23:43:07 +0200

    rewrite Entity to not use assert

--- src/Entity.ts
+++ src/Entity.ts
@@ -1,24 +1,29 @@
-import { assert } from './assert'
 import { Component, ComponentClass } from './Component'
 
 type ComponentMap = {
-  [key: string]: Component
+  [key: string]: Component | undefined
 }
 
 export class Entity {
   private _components: ComponentMap = Object.create(null)
   private _changeRegistered = false
 
-  constructor (private _registerChange) {
-  }
+  constructor (
+    private _registerChange: (entity: Entity) => void
+  ) {}
 
   add (instance: Component) {
-    assert(instance != null, 'Entity.add :: Argument is not a component instance.')
+    if (instance == null) {
+      throw new Error('Entity.add :: Argument is not a component instance.')
+    }
 
     const componentClass = instance.constructor
 
-    assert(componentClass && componentClass.id != null, 'Entity.add :: Argument is not a component instance.')
-    assert(!this._components[componentClass.id], 'Entity.add :: Component class already present.')
+    if (!componentClass || !componentClass.id) {
+      throw new Error('Entity.add :: Argument is not a component instance.')
+    } else if (this._components[componentClass.id]) {
+      throw new Error('Entity.add :: Component class already present.')
+    }
 
     this._components[componentClass.id] = instance
     this._onChange()
@@ -27,19 +32,31 @@ export class Entity {
   }
 
   has (componentClass: ComponentClass<any>) {
-    assert(componentClass && componentClass.id != null, 'Entity.has :: Argument is not a component class.')
+    if (!componentClass || !componentClass.id) {
+      throw new Error('Entity.has :: Argument is not a component class.')
+    }
+
     return !!this._components[componentClass.id]
   }
 
   get <T extends Component> (componentClass: ComponentClass<T>): T {
-    assert(componentClass && componentClass.id != null, 'Entity.get :: Argument is not a component class.')
+    if (!componentClass || !componentClass.id) {
+      throw new Error('Entity.get :: Argument is not a component class.')
+    }
+
     const component = this._components[componentClass.id]
-    assert(component != null, 'Entity.get :: Component class not present.')
+
+    if (!component) {
+      throw new Error('Entity.get :: Component class not present.')
+    }
+
     return <T>component
   }
 
   remove (componentClass: ComponentClass<any>) {
-    assert(componentClass && componentClass.id != null, 'Entity.remove :: Argument is not a component class.')
+    if (!componentClass || !componentClass.id) {
+      throw new Error('Entity.remove :: Argument is not a component class.')
+    }
 
     this._components[componentClass.id] = undefined
     this._onChange()
@@ -50,8 +67,8 @@ export class Entity {
   _onChange () {
     if (!this._changeRegistered) {
       this._registerChange(this)
+      this._changeRegistered = true
     }
-    this._changeRegistered = true
   }
 
   _onChangeRegistered () {
