85c1031bc55b35a07c9e3751ebe21e79a831eeb5
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2018-05-13 20:41:05 +0200

    selectors static typing

--- src/selectors.ts
+++ src/selectors.ts
@@ -1,19 +1,28 @@
-import { assert } from './assert'
+import { Entity } from './Entity'
+import { ComponentClass } from './Component'
 
-export function hasAll (...components) {
-  assert(components.every(isComponent), 'hasAll :: All arguments must be components.')
+export type Selector = (entity: Entity) => boolean
+
+export function hasAll (...components: ComponentClass<any>[]): Selector {
+  if (!components.every(isComponent)) {
+    throw new Error('hasAll :: All arguments must be components.')
+  }
+
   return entity => components.every(
     component => entity.has(component)
   )
 }
 
-export function hasAny (...components) {
-  assert(components.every(isComponent), 'hasAny :: All arguments must be components.')
+export function hasAny (...components: ComponentClass<any>[]): Selector {
+  if (!components.every(isComponent)) {
+    throw new Error('hasAny :: All arguments must be components.')
+  }
+
   return entity => components.some(
     component => entity.has(component)
   )
 }
 
-function isComponent (Component) {
-  return Component && Component.id
+function isComponent (componentClass: any): componentClass is ComponentClass<any> {
+  return componentClass && componentClass.id
 }
