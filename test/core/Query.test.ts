2cf717208dcc2e25414456a96da34e65b7e34330
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2018-07-18 14:31:30 +0200

    cover Query with tests

--- test/core/Query.test.ts
+++ test/core/Query.test.ts
@@ -1,4 +1,4 @@
-import { SingleQuery } from '../../src/core/Query'
+import { createQuery } from '../../src/core/Query'
 import { Entity } from '../../src/core/Entity'
 import { hasAll } from '../../src/core/selectors'
 
@@ -12,13 +12,23 @@ class ComponentB {
 
 const selector = hasAll(ComponentA)
 
-describe('Query', () => {
-  it('can be constructed', () => {
-    expect(() => new SingleQuery(selector)).not.toThrow()
+describe('createQuery', () => {
+  it('can create SingleQuery', () => {
+    expect(() => createQuery(selector)).not.toThrow()
   })
 
+  it('can create MultiQuery', () => {
+    expect(() => createQuery([selector, selector])).not.toThrow()
+  })
+
+  it('validates its arguments', () => {
+    expect(() => createQuery([null])).toThrow()
+  })
+})
+
+describe('SingleQuery', () => {
   test('onChange should correctly modify the entities list', () => {
-    const query = new SingleQuery(selector)
+    const query = createQuery(selector)
     const entity = new Entity()
       .add(new ComponentA())
 
@@ -38,7 +48,7 @@ describe('Query', () => {
   })
 
   test('onRemove should correctly modify the entities list', () => {
-    const query = new SingleQuery(selector)
+    const query = createQuery(selector)
     const entity = new Entity()
       .add(new ComponentA())
 
@@ -47,5 +57,63 @@ describe('Query', () => {
 
     query.onRemove(entity)
     expect(query.entities).toEqual([])
+  })
+
+  test('handles multiple entities', () => {
+    const query = createQuery(selector)
+
+    const entityA = new Entity().add(new ComponentA())
+    const entityB = new Entity().add(new ComponentA())
+
+    query.onChange(entityA)
+    query.onChange(entityB)
+    expect(query.entities).toEqual([entityA, entityB])
+
+    query.onRemove(entityA)
+    query.onRemove(entityB)
+    expect(query.entities).toEqual([])
+  })
+
+  test('handles unknown entities', () => {
+    const query = createQuery(selector)
+    const entity = new Entity()
+
+    query.onRemove(entity)
+
+    expect(query.entities).toEqual([])
+  })
+})
+
+describe('MultiQuery', () => {
+  test('onChange should correctly modify the entities list', () => {
+    const query = createQuery([selector, selector])
+    const entity = new Entity()
+      .add(new ComponentA())
+
+    query.onChange(entity)
+
+    expect(query.entities).toEqual([ [entity], [entity] ])
+
+    entity.add(new ComponentB())
+    query.onChange(entity)
+
+    expect(query.entities).toEqual([ [entity], [entity] ])
+
+    entity.remove(ComponentA)
+
+    query.onChange(entity)
+    expect(query.entities).toEqual([ [], [] ])
+  })
+
+  test('onRemove should correctly modify the entities list', () => {
+    const query = createQuery([selector, selector])
+    const entity = new Entity()
+      .add(new ComponentA())
+
+    query.onChange(entity)
+    expect(query.entities).toEqual([ [entity], [entity] ])
+
+    query.onRemove(entity)
+    expect(query.entities).toEqual([ [], [] ])
   })
 })
