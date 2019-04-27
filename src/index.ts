2b72d7d671d9d9ba17c7f92ccc43e1cdef1d0149
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2019-04-27 13:09:44 +0200

    add index to math

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,4 @@ export {
 export { Entity } from './Entity'
 export { Event, EventConstructor } from './Events'
 export { ComponentClass } from './components'
-export { lerp } from './math/lerp'
-export { clamp } from './math/clamp'
-export { Vector2 } from './math/Vector2'
+export * from './math'
